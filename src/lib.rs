f60cf8fe651415b7a2b5ae3078c647030408cca4
seancroach <me@seancroach.dev>
2023-01-21 22:15:19 -0800

    docs: complete the basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,67 @@
-//! TODO
+//! Read and write integers encoded using [LEB128].
+//!
+//! Unlike other LEB128 libraries there are three notable changes:
+//!
+//! 1. Uses [`zc_io`] instead of the standard library for `no_std`
+//!    compatability. The standard library can be used through
+//!    [`zc_io::IoReader`] and [`zc_io::IoWriter`] instances.
+//! 2. When writing encoded integers, an internal buffer on the stack is used to
+//!    possibly reduce system calls; each encoded integer makes a single call to
+//!    [`write_all`]. This is particularly useful since buffered writers are
+//!    frequently underutilized (and not native to [`zc_io`]).
+//! 3. Methods always return how many bytes were used when reading or writing
+//!    the integers, which may help in instances where that information would
+//!    have to get retrospectively computed.
+//!
+//! If none of these changes are meaningful to you, consider another LEB128
+//! project, as they would have less friction when just using the standard
+//! library's I/O interfaces.
+//!
+//! # Examples
+//!
+//! Read and write unsigned integers:
+//!
+//! ```
+//! # fn main() -> zc_io::Result<()> {
+//! let mut buf = [0; 5];
+//!
+//! let encoded_length = mini_leb128::write_u32(buf.as_mut_slice(), 624_485)?;
+//! assert_eq!(encoded_length.get(), 3);
+//! assert_eq!(buf, [0xE5, 0x8E, 0x26, 0x00, 0x00]);
+//!
+//! let (value, bytes_read) = mini_leb128::read_u32(buf.as_slice())?;
+//! assert_eq!(value, 624_485);
+//! assert_eq!(bytes_read.get(), 3);
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! Read and write signed integers:
+//!
+//! ```
+//! # fn main() -> zc_io::Result<()> {
+//! let mut buf = [0; 5];
+//!
+//! let encoded_length = mini_leb128::write_i32(buf.as_mut_slice(), -123_456)?;
+//! assert_eq!(encoded_length.get(), 3);
+//! assert_eq!(buf, [0xC0, 0xBB, 0x78, 0x00, 0x00]);
+//!
+//! let (value, bytes_read) = mini_leb128::read_i32(buf.as_slice())?;
+//! assert_eq!(value, -123_456);
+//! assert_eq!(bytes_read.get(), 3);
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! [LEB128]: https://en.wikipedia.org/wiki/LEB128
+//! [`zc_io::IoReader`]: https://docs.rs/zc_io/latest/zc_io/struct.IoReader.html
+//! [`zc_io::IoWriter`]: https://docs.rs/zc_io/latest/zc_io/struct.IoWriter.html
+//! [`write_all`]: Write::write_all
 
 #![no_std]
 #![deny(missing_docs, clippy::pedantic)]
 
-use core::mem;
-use core::num::NonZeroUsize;
+use core::{mem, num::NonZeroUsize};
 
 use arrayvec::ArrayVec;
 use zc_io::{error, Error, Read, Result, Write};
@@ -49,11 +106,15 @@ macro_rules! write_unsigned {
     }};
 }
 
-/// TODO
+/// Encodes an unsigned 32-bit integer using LEB128.
+///
+/// Returns a [`NonZeroUsize`] that stores how many bytes it took to encode the
+/// integer.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the writer. However, encoding the
+/// integer itself is infallible.
 pub fn write_u32<W>(mut writer: W, mut n: u32) -> Result<NonZeroUsize>
 where
     W: Write,
@@ -61,11 +122,15 @@ where
     write_unsigned!(writer, n, u32)
 }
 
-/// TODO
+/// Encodes an unsigned 64-bit integer using LEB128.
+///
+/// Returns a [`NonZeroUsize`] that stores how many bytes it took to encode the
+/// integer.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the writer. However, encoding the
+/// integer itself is infallible.
 pub fn write_u64<W>(mut writer: W, mut n: u64) -> Result<NonZeroUsize>
 where
     W: Write,
@@ -73,11 +138,15 @@ where
     write_unsigned!(writer, n, u64)
 }
 
-/// TODO
+/// Encodes an unsigned 128-bit integer using LEB128.
+///
+/// Returns a [`NonZeroUsize`] that stores how many bytes it took to encode the
+/// integer.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the writer. However, encoding the
+/// integer itself is infallible.
 pub fn write_u128<W>(mut writer: W, mut n: u128) -> Result<NonZeroUsize>
 where
     W: Write,
@@ -110,18 +179,22 @@ macro_rules! write_signed {
             bytes_written += 1;
 
             if done {
-                $writer.write_all(&[byte])?;
+                $writer.write_all(&buf)?;
                 return Ok(unsafe { NonZeroUsize::new_unchecked(bytes_written) });
             }
         }
     }};
 }
 
-/// TODO
+/// Encodes a signed 32-bit integer using LEB128.
+///
+/// Returns a [`NonZeroUsize`] that stores how many bytes it took to encode the
+/// integer.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the writer. However, encoding the
+/// integer itself is infallible.
 pub fn write_i32<W>(mut writer: W, mut n: i32) -> Result<NonZeroUsize>
 where
     W: Write,
@@ -129,11 +202,15 @@ where
     write_signed!(writer, n, i32)
 }
 
-/// TODO
+/// Encodes a signed 64-bit integer using LEB128.
+///
+/// Returns a [`NonZeroUsize`] that stores how many bytes it took to encode the
+/// integer.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the writer. However, encoding the
+/// integer itself is infallible.
 pub fn write_i64<W>(mut writer: W, mut n: i64) -> Result<NonZeroUsize>
 where
     W: Write,
@@ -141,11 +218,15 @@ where
     write_signed!(writer, n, i64)
 }
 
-/// TODO
+/// Encodes a signed 128-bit integer using LEB128.
+///
+/// Returns a [`NonZeroUsize`] that stores how many bytes it took to encode the
+/// integer.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the writer. However, encoding the
+/// integer itself is infallible.
 pub fn write_i128<W>(mut writer: W, mut n: i128) -> Result<NonZeroUsize>
 where
     W: Write,
@@ -165,9 +246,9 @@ where
 /// For instance, a developer could test for the `InvalidData` error variant and
 /// substitute the value as the max that datatype could represent if it was not
 /// critical. Then, resuming the data stream, it wouldn't be in the middle of
-/// an oversized VLQ.
+/// an oversized integer.
 #[cold]
-fn discard_vlq<'a, R>(mut reader: R) -> Error
+fn discard_leb128<'a, R>(mut reader: R) -> Error
 where
     R: Read<'a>,
 {
@@ -209,17 +290,22 @@ macro_rules! read_unsigned {
             }
 
             if shift == SIZE * VALUE_LENGTH {
-                return Err(discard_vlq(&mut $reader));
+                return Err(discard_leb128(&mut $reader));
             }
         }
     }};
 }
 
-/// TODO
+/// Decodes an unsigned 32-bit integer using LEB128.
+///
+/// Returns a tuple of the integer read and a [`NonZeroUsize`] that stores the
+/// number of bytes read.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the reader. Otherwise, an
+/// error kind of `InvalidData` is thrown if the integer read does not fit in
+/// the integer's datatype.
 pub fn read_u32<'a, R>(mut reader: R) -> Result<(u32, NonZeroUsize)>
 where
     R: Read<'a>,
@@ -227,11 +313,16 @@ where
     read_unsigned!(reader, u32)
 }
 
-/// TODO
+/// Decodes an unsigned 64-bit integer using LEB128.
+///
+/// Returns a tuple of the integer read and a [`NonZeroUsize`] that stores the
+/// number of bytes read.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the reader. Otherwise, an
+/// error kind of `InvalidData` is thrown if the integer read does not fit in
+/// the integer's datatype.
 pub fn read_u64<'a, R>(mut reader: R) -> Result<(u64, NonZeroUsize)>
 where
     R: Read<'a>,
@@ -239,11 +330,16 @@ where
     read_unsigned!(reader, u64)
 }
 
-/// TODO
+/// Decodes an unsigned 128-bit integer using LEB128.
+///
+/// Returns a tuple of the integer read and a [`NonZeroUsize`] that stores the
+/// number of bytes read.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the reader. Otherwise, an
+/// error kind of `InvalidData` is thrown if the integer read does not fit in
+/// the integer's datatype.
 pub fn read_u128<'a, R>(mut reader: R) -> Result<(u128, NonZeroUsize)>
 where
     R: Read<'a>,
@@ -275,17 +371,22 @@ macro_rules! read_signed {
             }
 
             if shift == SIZE * VALUE_LENGTH {
-                return Err(discard_vlq(&mut $reader));
+                return Err(discard_leb128(&mut $reader));
             }
         }
     }};
 }
 
-/// TODO
+/// Decodes a signed 32-bit integer using LEB128.
+///
+/// Returns a tuple of the integer read and a [`NonZeroUsize`] that stores the
+/// number of bytes read.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the reader. Otherwise, an
+/// error kind of `InvalidData` is thrown if the integer read does not fit in
+/// the integer's datatype.
 pub fn read_i32<'a, R>(mut reader: R) -> Result<(i32, NonZeroUsize)>
 where
     R: Read<'a>,
@@ -293,11 +394,16 @@ where
     read_signed!(reader, i32)
 }
 
-/// TODO
+/// Decodes a signed 64-bit integer using LEB128.
+///
+/// Returns a tuple of the integer read and a [`NonZeroUsize`] that stores the
+/// number of bytes read.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the reader. Otherwise, an
+/// error kind of `InvalidData` is thrown if the integer read does not fit in
+/// the integer's datatype.
 pub fn read_i64<'a, R>(mut reader: R) -> Result<(i64, NonZeroUsize)>
 where
     R: Read<'a>,
@@ -305,11 +411,16 @@ where
     read_signed!(reader, i64)
 }
 
-/// TODO
+/// Decodes a signed 128-bit integer using LEB128.
+///
+/// Returns a tuple of the integer read and a [`NonZeroUsize`] that stores the
+/// number of bytes read.
 ///
 /// # Errors
 ///
-/// TODO
+/// Propagates any I/O errors originating from the reader. Otherwise, an
+/// error kind of `InvalidData` is thrown if the integer read does not fit in
+/// the integer's datatype.
 pub fn read_i128<'a, R>(mut reader: R) -> Result<(i128, NonZeroUsize)>
 where
     R: Read<'a>,
